ef141f3ce44fc236bf3bae3ae3235adf8f7aa618
Roseco-crs <rodolphe.segbedji.aims.ac.rw>
2024-04-16 19:43:47 +0100

    Opened a given website by:   - providing its URL to browser.goto() function

--- tasks.py
+++ tasks.py
@@ -1,9 +1,24 @@
 from robocorp.tasks import task
+from robocorp import browser 
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def extract_data_from_website():
+    """ Extract data from a new website """
+    browser.configure(
+        slowmo=100,
+    )   
+    open_the_website()
+    # log_in()
+
+def open_the_website():
+    """ Open the website by navigating to the given URL"""
+    # browser.goto("https://gothamist.com/")
+    browser.goto("https://robotsparebinindustries.com/")
+
+# def log_in():
+#     """ Fills in the login form and clicks on the 'Log in' button """
+#     page = browser.page()
+#     page.fill("#username", "maria")
 
 
 
